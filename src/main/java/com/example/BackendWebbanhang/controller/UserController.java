8795891c7b772cf63207b177a32c3d48afd8383b
Tran Quoc Hoa <tranquochoan349@gmail.com>
2026-01-10 18:01:17 +0700

    Mới tạo source code backend ngày đầu

---
+++ src/main/java/com/example/BackendWebbanhang/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.example.BackendWebbanhang.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class UserController {
+    @GetMapping("/")
+    public String getMethodName() {
+        return "test";
+    }
+}
